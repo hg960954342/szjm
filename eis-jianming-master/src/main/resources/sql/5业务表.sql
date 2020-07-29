9626631552d9bb887d303e19153e87dc42e19b55
chenbo@prolog-int.com <chenbo@prolog-int.com>
2020-07-29 10:24:47 +0800

    容器任务增加状态

--- eis-jianming-master/src/main/resources/sql/5业务表.sql
+++ eis-jianming-master/src/main/resources/sql/5业务表.sql
@@ -6,8 +6,9 @@ CREATE TABLE `container_task` (
   `source` varchar(50) NOT NULL COMMENT '当前位置',
   `target` varchar(50) NOT NULL COMMENT '目的地 可能是agv区域坐标，也可能是入库提升机',
   `target_type` varchar(50) NOT NULL COMMENT '1 agv区域 2输送线',
-  `tast_state` varchar(50) NOT NULL COMMENT '任务进度 每加一个，对应加一个时间字段',
+  `tast_state` varchar(50) NOT NULL COMMENT '1 到位 2移动中  后面可以扩充',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `last_update_time` datetime DEFAULT NULL COMMENT '最后一次更新时间',
   `end_time` datetime DEFAULT NULL COMMENT '结束时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='托盘任务表';
