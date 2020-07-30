fabc50ededb4141b6e7e2104410aa8de5bfb4dfe
chenbo@prolog-int.com <chenbo@prolog-int.com>
2020-07-30 15:05:38 +0800

    更新部分表结构

--- eis-jianming-master/src/main/resources/sql/5业务表.sql
+++ eis-jianming-master/src/main/resources/sql/5业务表.sql
@@ -6,10 +6,16 @@ CREATE TABLE `container_task` (
   `source_type` varchar(50) NOT NULL COMMENT '当前托盘区域 1托盘库内 2agv区域',
   `target` varchar(50) NOT NULL COMMENT '目的地 可能是agv区域坐标，也可能是入库提升机',
   `target_type` varchar(50) NOT NULL COMMENT '1 agv区域 2输送线',
-  `task_state` varchar(50) NOT NULL COMMENT '1 到位 2移动中  后面可以扩充',
+  `task_state` varchar(50) NOT NULL COMMENT '1 到位 2已发送给下游设备 3下游设备回告开始 4离开原存储位 后面可以扩充',
   `task_code` varchar(50) NOT NULL COMMENT '任务号',
+  `item_id` varchar(255) DEFAULT NULL COMMENT 'wms商品id',
+  `lot_id` varchar(255) DEFAULT NULL COMMENT 'wms批号',
+  `ownerid` varchar(255) DEFAULT NULL COMMENT 'wms业主',
+  `qty` decimal DEFAULT NULL COMMENT '数量（重量）',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
-  `last_update_time` datetime DEFAULT NULL COMMENT '最后一次更新时间',
+  `send_time` datetime DEFAULT NULL COMMENT '发送给设备时间',
+  `start_time` datetime DEFAULT NULL COMMENT '设备开始时间',
+  `move_time` datetime DEFAULT NULL COMMENT '离开原存储位时间',
   `end_time` datetime DEFAULT NULL COMMENT '结束时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='托盘任务表';
