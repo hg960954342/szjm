aa4a892a7e24875f3de3a2fa93f77e2d039c44a5
zhouchao <zhouchao@prolog-int.com>
2020-08-01 13:09:40 +0800

    添加ContainerTaskDetail对应的mapper及service

---
+++ eis-jianming-master/src/main/java/com/prolog/eis/dao/ContainerTaskDetailMapper.java
@@ -0,0 +1,7 @@
+package com.prolog.eis.dao;
+
+import com.prolog.eis.model.eis.ContainerTaskDetail;
+import com.prolog.framework.dao.mapper.BaseMapper;
+
+public interface ContainerTaskDetailMapper extends BaseMapper<ContainerTaskDetail> {
+}
