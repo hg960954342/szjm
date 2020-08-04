1cbe0d4a38bf5b79a14d83734c856c2914a9d243
chenbo@prolog-int.com <chenbo@prolog-int.com>
2020-08-04 10:16:48 +0800

    四向库库存，添加商品信息

--- eis-jianming-master/src/main/java/com/prolog/eis/util/detetionlayer/DetetionLayerHelper.java
+++ eis-jianming-master/src/main/java/com/prolog/eis/util/detetionlayer/DetetionLayerHelper.java
@@ -6,50 +6,14 @@ import java.util.List;
 public class DetetionLayerHelper {
 
 	public static List<List<Integer>> getLayers(int detection,int minLayer,int maxLayer) {
-		List<List<Integer>> layerGroups = new ArrayList<>();
 		
-		if(detection <= 1) {
-			List<Integer> layer1s = new ArrayList<Integer>();
-			if(minLayer <= 5 && maxLayer>=5) {
-				layer1s.add(5);
-			}
-			if(minLayer <= 6 && maxLayer>=6) {
-				layer1s.add(6);
-			}
-			if(!layer1s.isEmpty()) {
-				layerGroups.add(layer1s);	
-			}
+		//建民项目detection和分层无关
+		List<List<Integer>> layerGroups = new ArrayList<>();
+		List<Integer> layers = new ArrayList<Integer>();
+		for(int i=minLayer;i<maxLayer + 1;i++) {
+			layers.add(i);
 		}
-
-		if(detection <= 2) {
-			List<Integer> layer2s = new ArrayList<Integer>();
-			if(minLayer <= 7 && maxLayer>=7) {
-				layer2s.add(7);
-			}
-			if(!layer2s.isEmpty()) {
-				layerGroups.add(layer2s);	
-			}
-		}
-
-		if(detection <= 3) {
-			List<Integer> layer3s = new ArrayList<Integer>();
-			if(minLayer <= 3 && maxLayer>=3) {
-				layer3s.add(3);
-			}
-			if(!layer3s.isEmpty()) {
-				layerGroups.add(layer3s);
-			}
-		}
-
-		if(detection <= 4) {
-			List<Integer> layer4s = new ArrayList<Integer>();
-			if(minLayer <= 4 && maxLayer>=4) {
-				layer4s.add(4);	
-			}
-			if(!layer4s.isEmpty()) {
-				layerGroups.add(layer4s);
-			}
-		}
+		layerGroups.add(layers);
 		
 		return layerGroups;
 	}
