c795aa816929969deee6c940e1b019f50ddee416
chenzc@prolog-int.com <chenzc@prolog-int.com>
2020-08-05 13:52:24 +0800

    json转换工具类

---
+++ eis-jianming-master/src/main/java/com/prolog/eis/util/NameAndSimplePropertyPreFilter.java
@@ -0,0 +1,15 @@
+package com.prolog.eis.util;
+
+import com.alibaba.fastjson.serializer.NameFilter;
+import com.alibaba.fastjson.serializer.SerializeFilter;
+import com.alibaba.fastjson.serializer.SimplePropertyPreFilter;
+
+public class NameAndSimplePropertyPreFilter extends SimplePropertyPreFilter implements NameFilter, SerializeFilter {
+    public String process(Object source, String name, Object value) {
+        if (name == null || name.length() == 0) {
+            return name;
+        }
+        String pascalName = name.toLowerCase();
+        return pascalName;
+    }
+}
