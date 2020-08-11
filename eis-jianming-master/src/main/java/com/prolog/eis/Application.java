4f1ec21b8e5bb1f5ee4b7fdab638107e5de483b5
chenzc@prolog-int.com <chenzc@prolog-int.com>
2020-08-11 17:50:39 +0800

    加入联调访问日志监控

--- eis-jianming-master/src/main/java/com/prolog/eis/Application.java
+++ eis-jianming-master/src/main/java/com/prolog/eis/Application.java
@@ -1,12 +1,16 @@
 package com.prolog.eis;
 
+import com.prolog.eis.filter.LogFilter;
 import com.prolog.framework.authority.core.annotation.EnablePrologEmptySecurityServer;
+import com.prolog.framework.authority.core.interceptor.RestTemplateInterceptor;
 import com.prolog.framework.microservice.annotation.EnablePrologService;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -15,6 +19,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication()
 @EnableTransactionManagement
@@ -26,12 +32,24 @@ import java.nio.charset.StandardCharsets;
 @EnableAspectJAutoProxy
 public class Application {
 	@Bean
+	public FilterRegistrationBean registFilter() {
+		FilterRegistrationBean registration = new FilterRegistrationBean();
+		registration.setFilter(new LogFilter());
+		registration.addUrlPatterns("/*");
+		registration.setName("LogFilter");
+		registration.setOrder(1);
+		return registration;
+	}
+	@Bean
 	public RestTemplate restTemplate() {
 		HttpComponentsClientHttpRequestFactory httpRequestFactory = new HttpComponentsClientHttpRequestFactory();
 		httpRequestFactory.setConnectionRequestTimeout(5000);
 		httpRequestFactory.setConnectTimeout(5000);
 		httpRequestFactory.setReadTimeout(5000);
-		RestTemplate restTemplate = new RestTemplate(httpRequestFactory); 
+		RestTemplate restTemplate = new RestTemplate(httpRequestFactory);
+		List<ClientHttpRequestInterceptor> interceptors =new ArrayList<>();
+		//interceptors.add(new LogFilter());
+		restTemplate.setInterceptors(interceptors);
         restTemplate.getMessageConverters().set(1,new StringHttpMessageConverter(StandardCharsets.UTF_8)); // 支持中文编码
 		return restTemplate;
 	}
