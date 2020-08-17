1f4038870b6f647d07e8b558d3b01fe51d9b43e0
zhouchao <zhouchao@prolog-int.com>
2020-08-17 17:59:19 +0800

    将eis回告wms代码用try catch 包裹

--- eis-jianming-master/src/main/java/com/prolog/eis/service/EisCallbackService.java
+++ eis-jianming-master/src/main/java/com/prolog/eis/service/EisCallbackService.java
@@ -11,33 +11,33 @@ public interface EisCallbackService {
      * @param containerCode 托盘编号
      * @throws Exception
      */
-    void inBoundReport(String containerCode) throws Exception;
+    void inBoundReport(String containerCode) ;
 
     /**
      * eis 业务出库 回告 wms
      * @param containerTask
      * @throws Exception
      */
-    void outBoundReport(ContainerTask containerTask) throws Exception;
+    void outBoundReport(ContainerTask containerTask);
 
     /**
      * eis 移库出库 回告 wms
      * @param containerTask
      * @throws Exception
      */
-    void moveBoundReport(ContainerTask containerTask) throws Exception;
+    void moveBoundReport(ContainerTask containerTask);
 
     /**
      * eis 盘点出库 回告 wms
      * @param billNo 单据号
      * @throws Exception
      */
-    void checkBoundReport(String billNo) throws Exception;
+    void checkBoundReport(String billNo);
 
     /**
      * eis 重复回告 wms
      * @param repeatReport
      * @throws IOException
      */
-    void recall(RepeatReport repeatReport) throws IOException;
+    void recall(RepeatReport repeatReport);
 }
