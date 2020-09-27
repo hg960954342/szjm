8feecaad46b011807d9717969921e63f9e28cdd9
chengxudong2020 <872603935@qq.com>
2020-09-27 17:45:20 +0800

    代码重构

--- eis-jianming-master/src/main/resources/static/js/eis.js
+++ eis-jianming-master/src/main/resources/static/js/eis.js
@@ -1,8 +1,14 @@
-$(function () {
 
-$.extend({
-    eis:{}
-})
+(function(a){
+     a.eis={};
+     $.extend($.eis,{
+        getAgvJSON:function(){
+            var agvJSON= $.ajax({url:"agv.json",async:false}).responseText;
+            var json=JSON.parse(agvJSON);
+            return json;
+        },
+
+    });
     /**
      * 获取配置的Agv特殊点位
      */
@@ -25,21 +31,21 @@ $.extend({
             return point;
 
         },
-      isEmpty:function(obj){
-           if((typeof obj=='string')&&obj.constructor==String){
-               obj.replace(/(^\s*)|(\s*$)/g, '');
-               if (typeof obj === "undefined" || obj == null || obj.trim() == "") {
-                   return true;
-               } else {
-                   return false;
-               }
-           }
-           throw "输入的值不是字符串！";
+        isEmpty:function(obj){
+            if((typeof obj=='string')&&obj.constructor==String){
+                obj.replace(/(^\s*)|(\s*$)/g, '');
+                if (typeof obj === "undefined" || obj == null || obj.trim() == "") {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            throw "输入的值不是字符串！";
 
 
-      }
+        }
     })
 
 
+})(jQuery)
 
-});
